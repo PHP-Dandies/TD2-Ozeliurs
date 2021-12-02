402243ad8f75e02a633e62ec31ead0425f4e3427
BILLY Maxime <ozeliurs@gmail.com>
2021-12-02 13:55:32 +0000

    Create table

---
+++ db.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `USER` (
+  `ID` varchar(20),
+  `SEX` varchar(20),
+  `MAIL` varchar(20),
+  `PASS` varchar(20),
+  `PHONE` varchar(20),
+  `COUNTRY` varchar(20),
+  `CG` varchar(20)
+) ENGINE=InnoDB;
+
+COMMIT;
